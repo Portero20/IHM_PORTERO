8f710e272cde5930ac9c9eb6f30d548bf45c1f3c
Exequiel Portero <eportero421@gmail.com>
2022-07-24 12:19:14 -0300

    he agregado unos toasts

--- js/boton.js
+++ js/boton.js
@@ -7,3 +7,23 @@ if (toastTrigger) {
     toast.show()
   })
 }
+
+const toastTriggerr = document.getElementById('liveToastBtn2')
+const toastLiveExamplee = document.getElementById('liveToast2')
+if (toastTriggerr) {
+  toastTriggerr.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExamplee)
+
+    toast.show()
+  })
+}
+
+const toastTriggerrr = document.getElementById('liveToastBtn3')
+const toastLiveExampleee = document.getElementById('liveToast3')
+if (toastTriggerrr) {
+  toastTriggerrr.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExamplee)
+
+    toast.show()
+  })
+}
